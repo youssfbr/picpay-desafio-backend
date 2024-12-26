3148c069feaf0b31c573135f4c6c5d9f4e397e9e
Alisson Youssf <youssfbr@gmail.com>
2024-12-26 15:54:00 -0300

    Project created

---
+++ src/main/java/com/github/youssfbr/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.picpay_desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/picpay_desafio_backend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.picpay_desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
